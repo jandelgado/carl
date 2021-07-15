1b01607d7b7512cba507d5d2e417d625b4a58b78
jandelgado <jandelgado@users.noreply.github.com>
2021-07-15 23:30:40 +0200

    add --large-folders option (#13)

--- tools/renamer.sh
+++ tools/renamer.sh
@@ -1,47 +1,61 @@
 #!/bin/bash
-# Carl Music Box
+# (c) copyright 2021 by Jan Delgado
+# Carl Music Box SD card file renamer
 # a little tool to rename files in a directory for use with DFPlayer Mini's
 # folder mode. This tool renames a list of mp3-files into an ascending order,
 # i.e. 0001-<oldname>.mp3, 0002-<oldname>.mp3 etc. files already in this 
 # format are not touched. 
-# Usage:
-#   ./renamer.sh --dry-run /path/to/my/files/*mp3   - for the dry run
-#   ./renamer.sh /path/to/my/files/*mp3             - to rename the files
 #
 set -eou pipefail
-
-if [ "$1" == "--dry-run" ]; then
-    DRY_RUN=1
-    shift
-fi
-
-if [ $# -lt 1  ]; then
-    echo "usage: $0 [--dry-run] file [file]*"
-    exit 1
-fi
-
-# case insensitive regexp
 shopt -s nocasematch
+
+function usage {
+    echo "usage: $0 [--dry-run] [--large-folders] FILES"
+    echo "Examples:"
+    echo "  renamer.sh --dry-run /path/to/my/files/*mp3"
+    echo "  renamer.sh --large-folders /path/to/my/files/*mp3"
+    exit 1
+}
+
+NUM_DIGITS=3
+while [[ $# -ge 1 ]]; do
+    key="$1"
+    case $key in
+        --large-folders)
+            NUM_DIGITS=4
+            ;;
+        --dry-run)
+            DRY_RUN=1
+            ;;
+        -*)
+            usage 
+            ;;
+        *) break
+            ;;
+    esac
+    shift
+done
+
+[[ $# -lt 1 ]] && usage
 
 COUNT=1
 for fn in "$@"; do 
-
     EXT=${fn#*.}
 
     # consider only files with mp3 extension
     if [ "${EXT##*.}" != "mp3" ]; then
-        echo "warn: skipping '$fn' as not having mp3 extension"
+        echo "skipping '$fn' as not having mp3 extension"
         continue
     fi
     # ignore files already in correct format, e.g. 0003.mp3
-    if [[ $fn =~ ^(.*/)?[0-9]{4}.mp3$ ]]; then
-        echo "warn: skipping '$fn' as already in correct format."
+    if [[ $fn =~ ^(.*/)?[0-9]{$NUM_DIGITS}.mp3$ ]]; then
+        echo "skipping '$fn' as already in correct format."
         continue
     fi
 
     # make sure filename is not already in use and find next free slot.
     while : ; do
-        NEWNAME=$(printf "%04d" $COUNT)
+        NEWNAME=$(printf "%0${NUM_DIGITS}d" $COUNT)
         COUNT=$((COUNT +1))
         if [ ! -f "${NEWNAME}.mp3" ] && [ ! -f "${NEWNAME}.MP3" ]; then
             break
@@ -49,9 +63,8 @@ for fn in "$@"; do
     done
 
     if [ $DRY_RUN ]; then
-        echo mv "${fn}" "$NEWNAME.mp3"
+        echo mv \"${fn}\" \"$NEWNAME.mp3\"
     else 
         mv "${fn}" "$NEWNAME.mp3"
     fi
 done
-
